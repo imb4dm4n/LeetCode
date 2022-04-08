4f84a13f2749e0ee531f722657b39eba8e2c092e
badman <badman@bad.net>
2022-04-08 18:25:14 +0800

    update： linkList 116. Populating Next Right Pointers in Each Node

--- LeetCode/CLinkList.h
+++ LeetCode/CLinkList.h
@@ -390,5 +390,50 @@ namespace LinkList {
 			// return has_cycle;
 			
 		}
+		// https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
+		// 116. Populating Next Right Pointers in Each Node
+		/*You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the following definition:
+		Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+		Initially, all next pointers are set to NULL.
+		solution1: use queue. 直接超时。
+		*/
+		class Node {
+		public:
+			int val;
+			Node* left;
+			Node* right;
+			Node* next;
+
+			Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+			Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+			Node(int _val, Node* _left, Node* _right, Node* _next)
+				: val(_val), left(_left), right(_right), next(_next) {}
+		};
+		#include<queue>
+		using std::queue;
+		Node* connect(Node* root) {
+			if(!root)
+				return root;
+			queue<Node*> q;
+			q.push(root);
+			while(!q.empty()) {
+				int size = q.size();
+				Node* prev = nullptr;
+				while(size > 0) {
+					Node* head = q.front();
+					if(prev != nullptr)
+						prev->next = head;
+					prev = head;
+					size -= 1;
+					q.push(head->left);
+					q.push(head->right);
+				}
+			}
+			return root;
+		}
+
 	};
 };
