35b2a794b6bc05efcc8e15a299b53f1b37cabe17
badman <badman@bad.net>
2022-09-20 19:17:56 +0800

    update： 409. Longest Palindrome

--- python/Strings.py
+++ python/Strings.py
@@ -5,8 +5,61 @@ from typing import List
 from typing import Optional
 from unittest   import *
 import unittest
+import collections
 
 class Solution:
+    '''
+    # https://leetcode.com/problems/longest-palindrome/
+    # 409. Longest Palindrome
+    问题: 输入一个区分大小写的字符串, 返回由这些字符能构成的最长回文长度. ie : 输入 abccccdd 返回 7, 最长回文dccaccd
+    思路1: 本质就是求有多少个能成对的字符, 若还有单个字符, 则额外加1.
+    用一个dict保存每个字符出现个数, 再一次遍历, 统计成对的个数. 无法通过边界测试: ccc
+    边界测试: ccc; 
+    思路2: 需要把使用过的字符移出来. 
+    Runtime: 110 ms, faster than 5.58% of Python3 online submissions for Longest Palindrome.
+    Memory Usage: 13.9 MB, less than 66.55% of Python3 online submissions for Longest Palindrome.
+    '''
+    def longestPalindrome(self, s: str) -> int:
+        odds = sum(v & 1 for v in collections.Counter(s).values())
+        return len(s) - odds + bool(odds)
+        # 思路2:
+        l_tmp           =   list(s)
+        total           =   0
+        has_single      =   False
+        while l_tmp.__len__() > 0:
+            c       =   l_tmp.pop()
+            if c in l_tmp:
+                total   += 2
+                l_tmp.remove(c)
+                # print(l_tmp)
+                continue
+            else:
+                has_single  =   True
+        # print(l_tmp)
+        if has_single:
+            total       +=  1
+        return total
+
+        # 思路1: 无法通过边界测试: ccc
+        s_dict          =   {}
+        total           =   0   # 返回的个数
+        has_single      =   False# 最后是否有单个字符
+        for c in s:
+            if not s_dict.get(c):
+                s_dict[c]   =   1
+            else:
+                s_dict[c]   +=  1
+        
+        for c, count in s_dict.items():
+            if count > 1:
+                total       +=  count // 2 * 2
+            elif count == 1:
+                has_single  =   True
+        
+        if has_single:
+            total   +=  1
+        return total
+
     '''
     # https://leetcode.com/problems/longest-substring-without-repeating-characters/
     # 3. Longest Substring Without Repeating Characters
@@ -40,6 +93,13 @@ class Solution:
 # class testit(unittest.TestCase):
 #     def test_lengthOfLongestSubstring(self):
 solution        =   Solution()
+# 409. Longest Palindrome
+ret             =   solution.longestPalindrome("ccc")
+print(ret)
+ret             =   solution.longestPalindrome("abccccdd")
+print(ret)
+exit(0)
+# 3. Longest Substring Without Repeating Characters
 ret             =   solution.lengthOfLongestSubstring("abcabcbb")
 print(ret)
 ret             =   solution.lengthOfLongestSubstring("bbbbb")
